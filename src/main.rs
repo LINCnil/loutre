f657cc7e6e84acaf7bef8954ef77e17e6fa13c66
Rodolphe Bréard <rbreard@cnil.fr>
2024-09-05 17:13:14 +0200

    Resize the window

--- src/main.rs
+++ src/main.rs
@@ -36,8 +36,8 @@ const CONTENT_FILE_PATH_PREFIX: &str = "\\";
 const CONTENT_FILE_PATH_PREFIX: &str = "";
 
 const NB_FILES_START: u32 = 1;
-const WIN_WIDTH: f32 = 720.0;
-const WIN_HEIGHT: f32 = 345.0;
+const WIN_WIDTH: f32 = 820.0;
+const WIN_HEIGHT: f32 = 500.0;
 
 const MAIN_ROUNDING: f32 = 7.0;
 const UI_BTN_PADDING_H: f32 = 10.0;
