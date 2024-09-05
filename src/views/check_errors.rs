d50c9a6a3ff5b26b9c02de5d2c4e2ca17f15889c
Rodolphe Bréard <rbreard@cnil.fr>
2024-09-05 13:56:25 +0200

    Improve the check_errors view's display

--- src/views/check_errors.rs
+++ src/views/check_errors.rs
@@ -7,7 +7,9 @@ use eframe::egui::{self, Image};
 macro_rules! file_error {
 	($app: ident, $ui: ident, $lst: expr, $title: literal) => {
 		if !$lst.is_empty() {
-			$ui.label($app.i18n.msg($title));
+			$ui.add_space(super::UI_MARGIN_LARGE);
+			$ui.label($app.theme.title(&$app.i18n.msg($title)));
+			$ui.add_space(super::UI_EXTRA_SPACE);
 			for f in &$lst {
 				$ui.label(f.path.display().to_string());
 			}
@@ -25,16 +27,18 @@ pub fn display(app: &mut ChecksumApp, ui: &mut egui::Ui) {
 	ui.add(Image::from_bytes(logo_name, logo_bytes).fit_to_original_size(1.0));
 	ui.add_space(super::UI_MARGIN_LARGE);
 
-	if ui
-		.add(Button::new().text(app.i18n.msg("back")).render())
-		.clicked()
-	{
-		app.view = AppView::Main;
-	}
 	if let Some(CheckResult::CheckErrors(err)) = &app.file_check_result {
 		file_error!(app, ui, err.invalid_ctn_file, "title_invalid_ctn_file");
 		file_error!(app, ui, err.invalid_receipt, "title_invalid_receipt");
 		file_error!(app, ui, err.missing_ctn_file, "title_missing_ctn_file");
 		file_error!(app, ui, err.missing_receipt, "title_missing_receipt");
+	}
+
+	ui.add_space(super::UI_MARGIN_LARGE);
+	if ui
+		.add(Button::new().text(app.i18n.msg("back")).render())
+		.clicked()
+	{
+		app.view = AppView::Main;
 	}
 }
