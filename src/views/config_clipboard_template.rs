2cdceb0adc4ee89a09156062752bd4b4076fec03
Rodolphe Bréard <v.code.ngyvz7vf@what.tf>
2025-02-07 11:33:38 +0100

    Improve the live-preview

--- src/views/config_clipboard_template.rs
+++ src/views/config_clipboard_template.rs
@@ -4,8 +4,10 @@ use crate::app::Route;
 use crate::clipboard::ClipboardDefaultTemplate;
 use crate::components::{ApplyConfig, Button, Header, MainSection, Root};
 use crate::config::Config;
+use crate::templates::{filter_add_dir_level, filter_nb_letters, EntryTemplate};
 use dioxus::prelude::*;
 use dioxus_i18n::tid;
+use minijinja::{context, Environment};
 
 const EXAMPLE_NB_EVIDENCES: usize = 42;
 
@@ -15,7 +17,7 @@ pub fn ClipboardTemplateConfig(tpl_id: usize) -> Element {
 	let tpl = ClipboardDefaultTemplate::from_id(tpl_id).unwrap();
 	let mut cfg_sig = use_context::<Signal<Config>>();
 	let mut new_tpl = use_signal(|| tpl.get_template(&config, EXAMPLE_NB_EVIDENCES));
-	let mut preview = use_signal(&*new_tpl);
+	let mut preview = use_signal(|| gen_preview(new_tpl.to_string()));
 
 	rsx! {
 		Root {
@@ -38,7 +40,8 @@ pub fn ClipboardTemplateConfig(tpl_id: usize) -> Element {
 					}
 				}
 				div {
-					dangerous_inner_html: preview,
+					id: "view_config_clipboard_preview_area",
+					dangerous_inner_html: "{preview}",
 				}
 				ApplyConfig {
 					onclick: move |_event| {
@@ -94,6 +97,85 @@ pub fn ClipboardTemplateConfig(tpl_id: usize) -> Element {
 }
 
 fn gen_preview(input: String) -> String {
-	// TODO
-	input
+	#[cfg(unix)]
+	let base_dir = "test".as_bytes().to_vec();
+	#[cfg(not(unix))]
+	let base_dir = "test".to_string();
+	#[cfg(unix)]
+	let relative_path = "test".as_bytes().to_vec();
+	#[cfg(not(unix))]
+	let relative_path = "test".to_string();
+	let mut env = Environment::new();
+	env.add_filter("add_dir_level", filter_add_dir_level);
+	env.add_filter("nb_letters", filter_nb_letters);
+	let ctx = context!(
+		hash_func => "SHA-256",
+		nb_evidences => 3,
+		nb_start => 1,
+		evidence => context!(
+			name => "test.txt",
+			size => 42,
+			hash => "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08",
+			hash_func => "SHA-256",
+		),
+		evidences => vec![
+			EntryTemplate {
+				base_dir: base_dir.clone(),
+				relative_path: relative_path.clone(),
+				name: "test".to_string(),
+				is_dir: true,
+				is_file: false,
+				size: 2,
+				hash: "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855".to_string(),
+				hash_func: "SHA-256".to_string(),
+				evidences: vec![
+					EntryTemplate {
+						base_dir: base_dir.clone(),
+						relative_path: relative_path.clone(),
+						name: "asdf.txt".to_string(),
+						is_dir: false,
+						is_file: true,
+						size: 4,
+						hash: "4ba5df017a3d79d1e4ed92a83e38c20eacaf76b42486a199c91ac5bbb70c73f4".to_string(),
+						hash_func: "SHA-256".to_string(),
+						evidences: Vec::new(),
+					},
+					EntryTemplate {
+						base_dir: base_dir.clone(),
+						relative_path: relative_path.clone(),
+						name: "space.txt".to_string(),
+						is_dir: false,
+						is_file: true,
+						size: 2,
+						hash: "5fe719893f2a0957435a95aa75dbe82e637e3806805c2cef9bc5836baeaf9ff7".to_string(),
+						hash_func: "SHA-256".to_string(),
+						evidences: Vec::new(),
+					},
+				],
+			},
+			EntryTemplate {
+				base_dir,
+				relative_path,
+				name: "some file.txt".to_string(),
+				is_dir: false,
+				is_file: true,
+				size: 42,
+				hash: "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08".to_string(),
+				hash_func: "SHA-256".to_string(),
+				evidences: Vec::new(),
+			},
+		],
+	);
+	if let Err(e) = env.add_template("preview", &input) {
+		tracing::error!("Unable to load template: {e}");
+		return String::new();
+	}
+	let tmpl = env.get_template("preview").unwrap();
+	match tmpl.render(&ctx) {
+		Ok(s) => s,
+		Err(e) => {
+			tracing::error!("Unable to render template: {e}");
+			String::new()
+		}
+	}
 }
